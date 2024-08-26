a5a4ecfb3870d61aeecb88e32833ee6db04cb557
kiri-1 <belenjesuslp@gmail.com>
2024-08-26 16:53:09 -0300

    ejercicio 1 tp2

---
+++ index.js
@@ -0,0 +1,8 @@
+//TP2-Ejercicio 1 
+
+const libro = {
+    titulo: 'Don Quijote de la Mancha',
+    autor: 'Miguel de Cervantes',
+    añoDePublicacion: 1605
+}
+console.log('titulo: ' + libro.titulo + ', autor: ' + libro.autor + ', año de publicacion: ' + libro.añoDePublicacion);
